3d6f7dbcec3ded370150bc4585bc36ec67118ed3
chirayukong <chirayu.kong@gmail.com>
2017-03-27 12:07:35 -0400

    Updated indentation

--- src/main/java/edu/pitt/dbmi/ccd/rest/client/service/AbstractRequestService.java
+++ src/main/java/edu/pitt/dbmi/ccd/rest/client/service/AbstractRequestService.java
@@ -25,85 +25,86 @@ import edu.pitt.dbmi.ccd.rest.client.dto.user.JsonWebToken;
  */
 public class AbstractRequestService implements RestRequestService {
 
-    protected final String scheme;
-    protected final String hostname;
-    protected final int port;
+	protected final String scheme;
+	protected final String hostname;
+	protected final int port;
 
-    protected final CloseableHttpClient httpClient;
-    protected final HttpClientContext localContext;
+	protected final CloseableHttpClient httpClient;
+	protected final HttpClientContext localContext;
 
-    public AbstractRequestService(RestHttpsClient restHttpsClient,
-	    String scheme, String hostname, int port) {
-	this.httpClient = restHttpsClient.getHttpClient();
-	this.localContext = restHttpsClient.getLocalContext();
-	this.scheme = scheme;
-	this.hostname = hostname;
-	this.port = port;
-    }
-    
-    protected CloseableHttpResponse doGet(URI uri) throws ClientProtocolException, IOException{
-	HttpGet httpget = new HttpGet(uri);
-	httpget.addHeader(HttpHeaders.ACCEPT, "application/json");
-	System.out.println("Executing request " + httpget.getRequestLine());
-	return httpClient.execute(httpget, localContext);
-    }
-    
-    protected CloseableHttpResponse doGet(URI uri, JsonWebToken jsonWebToken) throws ClientProtocolException, IOException{
-	HttpGet httpget = new HttpGet(uri);
-	httpget.addHeader(HttpHeaders.ACCEPT, "application/json");
-	httpget.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jsonWebToken.getJwt());
-	System.out.println("Executing request " + httpget.getRequestLine());
-	
-	CloseableHttpResponse response = httpClient.execute(httpget);
-	/*System.out.println("----------------------------------------");
-
-	Header[] header = response.getAllHeaders();
-	for (int i = 0; i < header.length; i++) {
-	    String name = header[i].getName();
-	    String value = header[i].getValue();
-	    System.out.println(name + ":" + value);
+	public AbstractRequestService(RestHttpsClient restHttpsClient, String scheme, String hostname, int port) {
+		this.httpClient = restHttpsClient.getHttpClient();
+		this.localContext = restHttpsClient.getLocalContext();
+		this.scheme = scheme;
+		this.hostname = hostname;
+		this.port = port;
 	}
-	System.out.println("----------------------------------------");*/
 
-	return response;
-    }
-
-    protected CloseableHttpResponse doPost(URI uri, JsonWebToken jsonWebToken) throws ClientProtocolException, IOException{
-	return doPost(uri, null, jsonWebToken);
-    }
-    
-    protected CloseableHttpResponse doPost(URI uri, HttpEntity entity, JsonWebToken jsonWebToken) throws ClientProtocolException, IOException{
-	HttpPost httppost = new HttpPost(uri);
-	httppost.addHeader(HttpHeaders.ACCEPT, "application/json");
-	httppost.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jsonWebToken.getJwt());
-	System.out.println("Executing request " + httppost.getRequestLine());
-	if(entity != null){
-	    httppost.setEntity(entity);
+	protected CloseableHttpResponse doGet(URI uri) throws ClientProtocolException, IOException {
+		HttpGet httpget = new HttpGet(uri);
+		httpget.addHeader(HttpHeaders.ACCEPT, "application/json");
+		System.out.println("Executing request " + httpget.getRequestLine());
+		return httpClient.execute(httpget, localContext);
 	}
-	
-	CloseableHttpResponse response = httpClient.execute(httppost);
-	/*System.out.println("----------------------------------------");
 
-	Header[] header = response.getAllHeaders();
-	for (int i = 0; i < header.length; i++) {
-	    String name = header[i].getName();
-	    String value = header[i].getValue();
-	    System.out.println(name + ":" + value);
+	protected CloseableHttpResponse doGet(URI uri, JsonWebToken jsonWebToken)
+			throws ClientProtocolException, IOException {
+		HttpGet httpget = new HttpGet(uri);
+		httpget.addHeader(HttpHeaders.ACCEPT, "application/json");
+		httpget.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jsonWebToken.getJwt());
+		System.out.println("Executing request " + httpget.getRequestLine());
+
+		CloseableHttpResponse response = httpClient.execute(httpget);
+		/*
+		 * System.out.println("----------------------------------------");
+		 * 
+		 * Header[] header = response.getAllHeaders(); for (int i = 0; i <
+		 * header.length; i++) { String name = header[i].getName(); String value
+		 * = header[i].getValue(); System.out.println(name + ":" + value); }
+		 * System.out.println("----------------------------------------");
+		 */
+
+		return response;
 	}
-	System.out.println("----------------------------------------");*/
 
-	return response;
-    }
+	protected CloseableHttpResponse doPost(URI uri, JsonWebToken jsonWebToken)
+			throws ClientProtocolException, IOException {
+		return doPost(uri, null, jsonWebToken);
+	}
 
-    protected CloseableHttpResponse doDelete(URI uri, JsonWebToken jsonWebToken) throws ClientProtocolException, IOException{
-	HttpDelete httpDelete = new HttpDelete(uri);
-	httpDelete.addHeader(HttpHeaders.ACCEPT, "application/json");
-	httpDelete.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jsonWebToken.getJwt());
-	System.out.println("Executing request " + httpDelete.getRequestLine());
-	
-	CloseableHttpResponse response = httpClient.execute(httpDelete);
-	
-	return response;
-    }
-    
+	protected CloseableHttpResponse doPost(URI uri, HttpEntity entity, JsonWebToken jsonWebToken)
+			throws ClientProtocolException, IOException {
+		HttpPost httppost = new HttpPost(uri);
+		httppost.addHeader(HttpHeaders.ACCEPT, "application/json");
+		httppost.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jsonWebToken.getJwt());
+		System.out.println("Executing request " + httppost.getRequestLine());
+		if (entity != null) {
+			httppost.setEntity(entity);
+		}
+
+		CloseableHttpResponse response = httpClient.execute(httppost);
+		/*
+		 * System.out.println("----------------------------------------");
+		 * 
+		 * Header[] header = response.getAllHeaders(); for (int i = 0; i <
+		 * header.length; i++) { String name = header[i].getName(); String value
+		 * = header[i].getValue(); System.out.println(name + ":" + value); }
+		 * System.out.println("----------------------------------------");
+		 */
+
+		return response;
+	}
+
+	protected CloseableHttpResponse doDelete(URI uri, JsonWebToken jsonWebToken)
+			throws ClientProtocolException, IOException {
+		HttpDelete httpDelete = new HttpDelete(uri);
+		httpDelete.addHeader(HttpHeaders.ACCEPT, "application/json");
+		httpDelete.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jsonWebToken.getJwt());
+		System.out.println("Executing request " + httpDelete.getRequestLine());
+
+		CloseableHttpResponse response = httpClient.execute(httpDelete);
+
+		return response;
+	}
+
 }
