10cf155c35c66c76c35349e1c79375c4fa84056d
chirayukong <chirayu.kong@gmail.com>
2017-03-30 12:12:38 -0400

    Added log4j2 properties

--- src/main/java/edu/pitt/dbmi/ccd/rest/client/RestHttpsClient.java
+++ src/main/java/edu/pitt/dbmi/ccd/rest/client/RestHttpsClient.java
@@ -25,75 +25,68 @@ import org.apache.http.ssl.SSLContextBuilder;
 
 public class RestHttpsClient {
 
-    private final String email;
-    private final String password;
-    private final String scheme;
-    private final String hostname;
-    private final int port;
+	private final String email;
+	private final String password;
+	private final String scheme;
+	private final String hostname;
+	private final int port;
 
-    private final CloseableHttpClient httpClient;
-    private final HttpClientContext localContext;
-    
-    public RestHttpsClient(String email, String password, String scheme,
-	    String hostname, int port) throws Exception {
-	this.email = email;
-	this.password = password;
-	this.scheme = scheme;
-	this.hostname = hostname;
-	this.port = port;
-	this.httpClient = createHttpClient();
-	this.localContext = establishCustomContext(httpClient);
-    }
+	private final CloseableHttpClient httpClient;
+	private final HttpClientContext localContext;
 
-    private CloseableHttpClient createHttpClient() throws Exception {
-	// Trust own CA and all self-signed certs
-	SSLContextBuilder builder = new SSLContextBuilder();
-	builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+	public RestHttpsClient(String email, String password, String scheme, String hostname, int port) throws Exception {
+		this.email = email;
+		this.password = password;
+		this.scheme = scheme;
+		this.hostname = hostname;
+		this.port = port;
+		this.httpClient = createHttpClient();
+		this.localContext = establishCustomContext(httpClient);
+	}
 
-	SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-		builder.build(),
-		new String[] { "TLSv1.2" },
-		null,
-		SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+	private CloseableHttpClient createHttpClient() throws Exception {
+		// Trust own CA and all self-signed certs
+		SSLContextBuilder builder = new SSLContextBuilder();
+		builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
 
-	CredentialsProvider credsProvider = new BasicCredentialsProvider();
-	credsProvider.setCredentials(new AuthScope(hostname, port),
-		new UsernamePasswordCredentials(email, password));
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build(), new String[] { "TLSv1.2" },
+				null, SSLConnectionSocketFactory.getDefaultHostnameVerifier());
 
-	CloseableHttpClient httpClient = HttpClients.custom()
-		.setSSLSocketFactory(sslsf)
-		.setDefaultCredentialsProvider(credsProvider).build();
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(new AuthScope(hostname, port), new UsernamePasswordCredentials(email, password));
 
-	return httpClient;
-    }
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf)
+				.setDefaultCredentialsProvider(credsProvider).build();
 
-    private HttpClientContext establishCustomContext(
-	    CloseableHttpClient httpclient) throws Exception {
-	HttpHost target = new HttpHost(hostname, port, scheme);
+		return httpClient;
+	}
 
-	// Create AuthCache instance
-	AuthCache authCache = new BasicAuthCache();
-	// Generate BASIC scheme object and add it to the local
-	// auth cache
-	BasicScheme basicAuth = new BasicScheme();
-	authCache.put(target, basicAuth);
+	private HttpClientContext establishCustomContext(CloseableHttpClient httpclient) throws Exception {
+		HttpHost target = new HttpHost(hostname, port, scheme);
 
-	// Add AuthCache to the execution context
-	HttpClientContext localContext = HttpClientContext.create();
-	localContext.setAuthCache(authCache);
-	
-	return localContext;
-    }
+		// Create AuthCache instance
+		AuthCache authCache = new BasicAuthCache();
+		// Generate BASIC scheme object and add it to the local
+		// auth cache
+		BasicScheme basicAuth = new BasicScheme();
+		authCache.put(target, basicAuth);
 
-    public CloseableHttpClient getHttpClient() {
-	return httpClient;
-    }
+		// Add AuthCache to the execution context
+		HttpClientContext localContext = HttpClientContext.create();
+		localContext.setAuthCache(authCache);
 
-    public HttpClientContext getLocalContext() {
-	return localContext;
-    }
+		return localContext;
+	}
 
-    public static void main(String[] args) throws Exception {
+	public CloseableHttpClient getHttpClient() {
+		return httpClient;
+	}
 
-    }
+	public HttpClientContext getLocalContext() {
+		return localContext;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+	}
 }
